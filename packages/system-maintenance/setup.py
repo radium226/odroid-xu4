b2d4b1424effdb30b52f232c57a4cf514c12edfd
radium <r@di.um>
2018-09-18 11:30:15 +0200

    Review build system

--- packages/system-maintenance/setup.py
+++ packages/system-maintenance/setup.py
@@ -1,11 +1,25 @@
 #!/bin/env python
 
+import os
 from setuptools import setup
 
+def version():
+    try:
+        return os.environ["PKGVER"]
+    except:
+        with open("./VERSION", "r") as f:
+            return f.read().strip()
+
+def name():
+    try:
+        return os.environ["PKGNAME"]
+    except:
+        return os.path.basename(os.getcwd())
+
 setup(
-    name="system-maintenance",
-    version="1.0",
-    description="Some timers for Yay",
+    name=name(),
+    version=version(),
+    description="System Maintenance",
     license="GPL",
     zip_safe=True,
     install_requires=[],
