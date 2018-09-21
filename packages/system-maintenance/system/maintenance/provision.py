a8d8adc5f8c32a1288b296368a7a96888e5caecc
radium <r@di.um>
2018-09-21 13:50:50 +0200

    Simplify stuff

--- packages/system-maintenance/system/maintenance/provision.py
+++ packages/system-maintenance/system/maintenance/provision.py
@@ -5,7 +5,7 @@ from tempfile import mkdtemp
 
 from .context import Context
 
-from system.tools.ansible import Ansible, Inventory
+from system.tools.ansible import Ansible
 from system.tools.git import Git
 
 def provision(prompt):
@@ -15,5 +15,5 @@ def provision(prompt):
     Git.clone(context.config["provision"]["git_repo_url"], repo_folder_path)
     provisioning_folder_path = repo_folder_path / context.config["provision"]["folder_path"]
     host_name = context.config["provision"]["host_name"]
-    inventory = Inventory.read(provisioning_folder_path / context.config["provision"]["inventory_file_path"])#.modify_host(host_name, ansible_connection="local", ansible_port=None, ansible_host=None)
-    Ansible(inventory).run_playbook(provisioning_folder_path / context.config["provision"]["playbook_file_path"], limit=[host_name], local=True)
+    inventory_file_path = provisioning_folder_path / context.config["provision"]["inventory_file_path"]
+    Ansible(inventory_file_path).run_playbook(provisioning_folder_path / context.config["provision"]["playbook_file_path"], limit=[host_name], local=True)
--- packages/system-maintenance/system/tools/ansible/ansible.py
+++ packages/system-maintenance/system/tools/ansible/ansible.py
@@ -4,20 +4,15 @@ from subprocess import Popen
 
 class Ansible:
 
-    def __init__(self, inventory):
-        self.inventory = inventory
-
-    def modify_host(self, host_name, **vars):
-        new_inventory = self.inventory.modify_host(host_name, **vars)
-        return Ansible(new_inventory)
+    def __init__(self, inventory_file_path):
+        self.inventory_file_path = inventory_file_path
 
     def run_playbook(self, playbook_file_path, limit=[], local=False):
-        inventory_file_path = self.inventory.write(temp=True)
-        inventory_args = [f"--inventory-file={str(inventory_file_path)}"]
+        inventory_args = [f"--inventory-file={str(self.inventory_file_path)}"]
         limit_args = [f"--limit={','.join(limit)}"] if len(limit) > 0 else []
         connection_args = ["--connection=local"] if local else []
         playbook_args = [str(playbook_file_path)]
         command = ["ansible-playbook"] + inventory_args + playbook_args + connection_args + limit_args
-        print(f"command={command}")
+        #print(f"command={command}")
         process = Popen(command)
         process.wait()
